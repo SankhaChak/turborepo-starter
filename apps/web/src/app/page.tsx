b5af803e17684f29e8dfdb2f0ec1811755483f16
SankhaChak <anonproductionspro@gmail.com>
2024-08-02 16:44:04 +0530

    chore: ⚓ configure shared utils

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,13 @@
 import { Button } from "@repo/ui/components/ui/button";
+import { Something } from "@repo/utils/types/sample";
 
 export default function Page(): JSX.Element {
+  const something: Something = {
+    id: 1,
+    name: "Cool",
+  };
+  console.log("🚀 ~ file: page.tsx:9 ~ Page ~ something:", something);
+
   return (
     <main className="min-h-screen flex items-center justify-center">
       <Button>Something Cool</Button>
---
+++ packages/utils/types/sample.ts
@@ -0,0 +1,4 @@
+export type Something = {
+  id: number;
+  name: string;
+};
